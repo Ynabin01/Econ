bb805339a9e366d340a9cd84d5d716ec26b12ed1
Ynabin01 <nabin.01least@gmail.com>
2022-08-21 14:02:27 +0545

    newsevent

--- resources/views/website/newsevent.blade.php
+++ resources/views/website/newsevent.blade.php
@@ -12,12 +12,9 @@
             <div class="col-md-7">
                 <div class="news-list">
                 <ul>
-                    <li><a href="inner.html">13th Annual General Meeting concluded</a></li>
-                    <li><a href="inner.html">ECON calls to use ASYCUDA System</a></li>
-                    <li><a href="inner.html"> 18th AGM of the Council on shrawan 14,2072</a></li>
-                    <li><a href="inner.html"> Interaction and discussion program on export procedure</a></li>
-                    <li><a href="inner.html">19th AGM and Export Day 2073</a></li>
-
+                    @foreach ($newsevents as $newsevent)
+                      <li><a href="inner.html">{{$newsevent->short_content}}</a></li>
+                    @endforeach
                 </ul>
                 <a href="news-list" class="thm-btn about_one__btn">View All</a>
                 </div>

8f923db7cd38182262943e3f8b627c3eda6a5ca1
Ynabin01 <nabin.01least@gmail.com>
2022-08-24 15:12:13 +0545

    add

--- resources/views/website/memberform.blade.php
+++ resources/views/website/memberform.blade.php
@@ -29,19 +29,19 @@
                             <div class="col-md-4">
                                 <div class="text-input">
                                     <label for="govRegdDate">Gov. Regd. Date:</label>
-                                    <input type="date" name="govRegdDate" id="govRegdDate" required>
+                                    <input type="date" name="gov_reg_date" id="govRegdDate" required>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="text-input">
                                     <label for="ownerName">Owner's Name:</label>
-                                    <input type="text" name="ownerName" id="ownerName" required>
+                                    <input type="text" name="owner_name" id="ownerName" required>
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="text-input">
                                     <label for="companyName">Name of the Firm/Company:</label>
-                                    <input type="text" name="companyName" id="companyName">
+                                    <input type="text" name="company_name" id="companyName">
                                 </div>
                             </div>
                             <div class="col-md-12">
@@ -49,13 +49,13 @@
                                     <label>Type of Firm/Company</label>
                                     <ul>
                                         <li>
-                                            <input type="radio" name="company" /><span>Proprietorship</span>
+                                            <input type="radio" name="company_type" /><span>Proprietorship</span>
                                         </li>
                                         <li>
-                                            <input type="radio" name="company" /><span>Partnership</span>
+                                            <input type="radio" name="company_type" /><span>Partnership</span>
                                         </li>
                                         <li>
-                                            <input type="radio" name="company" /><span>Pvt. Ltd.</span>
+                                            <input type="radio" name="company_type" /><span>Pvt. Ltd.</span>
                                         </li>
                                     </ul>
                                 </div>
@@ -65,16 +65,16 @@
                                     <label>Type of Business</label>
                                     <ul>
                                         <li>
-                                            <input type="radio" name="business" /><span>Export</span>
+                                            <input type="radio" name="business_type" /><span>Export</span>
                                         </li>
                                         <li>
-                                            <input type="radio" name="business" /><span>Trading</span>
+                                            <input type="radio" name="business_type" /><span>Trading</span>
                                         </li>
                                         <li>
-                                            <input type="radio" name="business" /><span>Manufacture</span>
+                                            <input type="radio" name="business_type" /><span>Manufacture</span>
                                         </li>
                                         <li>
-                                            <input type="radio" name="business" /><span>Others...</span>
+                                            <input type="radio" name="business_type" /><span>Others...</span>
                                         </li>
                                     </ul>
                                 </div>
@@ -84,22 +84,22 @@
                                     <label>Main Commodities</label>
                                     <ul>
                                         <li>
-                                            <input type="checkbox" name="commodities<" /> <span>Silver</span>
+                                            <input type="checkbox" name="main_commodities<" /> <span>Silver</span>
                                         </li>
                                         <li>
-                                            <input type="checkbox" name="commodities" /> <span>Pashmina</span>
+                                            <input type="checkbox" name="main_commodities" /> <span>Pashmina</span>
                                         </li>
                                         <li>
-                                            <input type="checkbox" name="commodities" /> <span>Carpet</span>
+                                            <input type="checkbox" name="main_commodities" /> <span>Carpet</span>
                                         </li>
                                         <li>
-                                            <input type="checkbox" name="commodities" /> <span>Garment</span>
+                                            <input type="checkbox" name="main_commodities" /> <span>Garment</span>
                                         </li>
                                         <li>
-                                            <input type="checkbox" name="commodities" /> <span>Handicraft</span>
+                                            <input type="checkbox" name="main_commodities" /> <span>Handicraft</span>
                                         </li>
                                         <li>
-                                            <input type="checkbox" name="commodities" /> <span>Ohers</span>
+                                            <input type="checkbox" name="main_commodities" /> <span>Ohers</span>
                                         </li>
                                     </ul>
                                 </div>
@@ -107,19 +107,19 @@
                             <div class="col-md-4">
                                 <div class="text-input">
                                     <label for="govRegdDate">Countries; Exported to:</label>
-                                    <input type="text" name="country" id="country">
+                                    <input type="text" name="exported_to" id="country">
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="text-input">
                                     <label for="address">Office Address:</label>
-                                    <input type="text" name="address" id="address">
+                                    <input type="text" name="office_address" id="address">
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="text-input">
                                     <label for="postalAddress">Postal Address:</label>
-                                    <input type="text" name="cpostalAddress" id="postalAddress">
+                                    <input type="text" name="postal_address	" id="postalAddress">
                                 </div>
                             </div>
                             <div class="col-md-12">
@@ -128,13 +128,13 @@
                             <div class="col-md-3">
                                 <div class="text-input">
                                     <label for="telephone">Telephone No.</label>
-                                    <input type="number" name="telephone" id="telephone">
+                                    <input type="number" name="telephone_no" id="telephone">
                                 </div>
                             </div>
                             <div class="col-md-3">
                                 <div class="text-input">
                                     <label for="faxNo">Fax No.</label>
-                                    <input type="number" name="faxNo" id="faxNo">
+                                    <input type="number" name="fax_no" id="faxNo">
                                 </div>
                             </div>
                             <div class="col-md-3">
@@ -155,7 +155,7 @@
                             <div class="col-md-3">
                                 <div class="text-input">
                                     <label for="regdNo">Regd. No.</label>
-                                    <input type="number" name="regdNo" id="regdNo">
+                                    <input type="number" name="regd_no" id="regdNo">
                                 </div>
                             </div>
                             <div class="col-md-3">
@@ -173,7 +173,7 @@
                             <div class="col-md-3">
                                 <div class="text-input">
                                     <label for="panNo">PAN No.</label>
-                                    <input type="number" name="panNo" id="panNo">
+                                    <input type="number" name="pan_no" id="panNo">
                                 </div>
                             </div>
                             <div class="col-md-12">
@@ -208,7 +208,7 @@
                                 <div class="check-boxes">
                                     <ul>
                                         <li>
-                                            <input type="checkbox" name="agree<" /> <span>I/We hereby certify that above
+                                            <input type="checkbox" name="accept_policies<" /> <span>I/We hereby certify that above
                                                 declared infromation are true. I/We agree to obey the rule, regulation and
                                                 constiution of the council.</span>
                                         </li>

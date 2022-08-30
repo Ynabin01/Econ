80378945cf4571ae5f3e8d2fd41c94757d2e12e0
Ynabin01 <nabin.01least@gmail.com>
2022-08-30 17:48:04 +0545

    add

--- resources/views/website/newsevent.blade.php
+++ resources/views/website/newsevent.blade.php
@@ -16,7 +16,7 @@
                       <li><a href="inner.html">{{$newsevent->short_content}}</a></li>
                     @endforeach
                 </ul>
-                <a href="/newsevent" class="thm-btn about_one__btn">View All</a>
+                <a href="/news-events" class="thm-btn about_one__btn">View All</a>
                 </div>
             </div>
         </div>

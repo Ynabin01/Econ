1ef72c4ee26dea069c7c23377b5fc9513d555607
Ynabin01 <nabin.01least@gmail.com>
2022-09-01 15:20:35 +0545

    add

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -123,10 +123,10 @@ if (isset($normal)) {
                     </div>
                     <div class="topbar-one__right">
                         <div class="topbar-one__social">
-                            <a href="{{ $global_setting->facebook ?? '' }}"><i class="fab fa-facebook-square"></i></a>
-                            <a href="{{ $global_setting->twitter ?? '' }}"><i class="fab fa-twitter"></i></a>
-                            <a href="#"><i class="fab fa-linkedin"></i></a>
-                            <a href="#"><i class="fab fa-dribbble"></i></a>
+                            <a href="{{ $global_setting->facebook ?? '' }}" target="_blank" ><i class="fab fa-facebook-square"></i></a>
+                            <a href="{{ $global_setting->twitter ?? '' }}" target="_blank"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-linkedin" target="_blank"></i></a>
+                            <a href="#"><i class="fab fa-dribbble" target="_blank"></i></a>
                         </div>
                     </div>
                 </div>
